56b372fb4b8a7844447bd78e614b281ae2c029f9
yxu1183 <yupadhayaya08@gmail.com>
2020-11-25 21:40:36 -0600

    adding registration page

--- app/src/main/java/com/example/edutrak/MainActivity.java
+++ app/src/main/java/com/example/edutrak/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.edutrak;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = (TextView) findViewById(R.id.text_signup);
+        textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,activity_registration.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this,"",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
