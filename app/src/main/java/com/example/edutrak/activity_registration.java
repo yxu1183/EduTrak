161e77ea465a3ff389dfb4114a49662420111f52
yxu1183 <yupadhayaya08@gmail.com>
2020-11-25 21:54:20 -0600

    adding cancel button

--- app/src/main/java/com/example/edutrak/activity_registration.java
+++ app/src/main/java/com/example/edutrak/activity_registration.java
@@ -1,14 +1,27 @@
 package com.example.edutrak;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class activity_registration extends AppCompatActivity {
-
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        button = (Button)findViewById(R.id.btn_cancel);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Toast.makeText(activity_registration.this,"Registration Cancelled!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
