a0068d7b420a37a6fc75a2b26e0974128f11c424
yxu1183 <yupadhayaya08@gmail.com>
2020-11-26 19:51:14 -0600

    upload picture works

--- app/src/main/java/com/example/edutrak/activity_registration.java
+++ app/src/main/java/com/example/edutrak/activity_registration.java
@@ -1,27 +1,119 @@
 package com.example.edutrak;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.os.Bundle;
 
 public class activity_registration extends AppCompatActivity {
-    public Button button;
+    public Button picture_button;
+    public ImageView picture_view;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        button = (Button)findViewById(R.id.btn_cancel);
-        button.setOnClickListener(new View.OnClickListener() {
+        picture_button = (Button) findViewById(R.id.chose_image_btn);
+        picture_view = (ImageView) findViewById(R.id.chose_image_view);
+
+        picture_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-                Toast.makeText(activity_registration.this,"Registration Cancelled!",Toast.LENGTH_LONG).show();
+                if (ContextCompat.checkSelfPermission(activity_registration.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(activity_registration.this, "Permission is already granted!",
+                            Toast.LENGTH_SHORT).show();
+                    pickImageFromGallery();
+                } else {
+                    storagePermissionRequest();
+                }
             }
         });
     }
+    private void storagePermissionRequest()
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE))
+            {
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission Required")
+                        .setMessage("This permission is required to access media and files.")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(activity_registration.this,
+                                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_CODE);
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create().show();
+            }
+            else
+                {
+                ActivityCompat.requestPermissions(this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_CODE);
+            }
+
+        }
+
+    private void pickImageFromGallery()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+                pickImageFromGallery();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode ==IMAGE_PICK_CODE)
+        {
+            picture_view.setImageURI(data.getData());
+        }
+    }
+
+    //        button = (Button)findViewById(R.id.btn_cancel);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//                Toast.makeText(activity_registration.this,"Registration Cancelled!",Toast.LENGTH_LONG).show();
+//            }
+//        });
 }
