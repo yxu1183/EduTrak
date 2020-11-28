8db3f2301220d19e15652ebb8bca94912fa172c7
yxu1183 <yupadhayaya08@gmail.com>
2020-11-28 12:18:17 -0600

    main homepage

--- app/src/main/java/com/example/edutrak/activity_homepage.java
+++ app/src/main/java/com/example/edutrak/activity_homepage.java
@@ -2,6 +2,7 @@ package com.example.edutrak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class activity_homepage extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class activity_homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+
     }
 }
